8f6a070601286b54efc78fcc7ac348e525ec6269
Hameeda6 <hameeda0115@gmail.com>
2022-02-21 22:18:07 -0700

    Encryption Test fail

---
+++ cypher.py
@@ -0,0 +1,31 @@
+import sys
+
+def encrypt(text,s):
+    result = ""
+   # transverse the plain text
+    for i in range(len(text)):
+      char = text[i]
+      # Encrypt uppercase characters in plain text
+      
+      if (char.isupper()):
+         result += chr((ord(char) + s-65) % 26 + 65)
+      # Encrypt lowercase characters in plain text
+      else:
+         result += chr((ord(char) + s - 97) % 26 + 97)
+
+    return result
+#check the above function
+
+
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+txt = "CEASER MAN is bad"
+a = 5
+num = 26-a
+
+encrypt_word = sys.argv[1]
+decrypt_word = sys.argv[2]
+
+
+print(" The encrypted text is: "+encrypt(encrypt_word, a))
+print(" The decrypted text is: "+encrypt(decrypt_word, num))
